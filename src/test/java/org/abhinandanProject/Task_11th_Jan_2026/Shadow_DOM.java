770ca3e98c473bc222f2f227d6c36da9f4cc542a
abhinandan998 <basuabhinandan99@gmail.com>
2026-01-11 22:16:11 +0530

    11th Jan

--- src/test/java/org/abhinandanProject/Task_11th_Jan_2026/Shadow_DOM.java
+++ src/test/java/org/abhinandanProject/Task_11th_Jan_2026/Shadow_DOM.java
@@ -1,11 +1,14 @@
 package org.abhinandanProject.Task_11th_Jan_2026;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.testng.annotations.Test;
+
+import java.util.Set;
 
 public class Shadow_DOM {
 
@@ -15,7 +18,10 @@ public class Shadow_DOM {
         driver.get("https://selectorshub.com/xpath-practice-page/");
         driver.manage().window().maximize();
 
+
         JavascriptExecutor js = (JavascriptExecutor) driver;
+
+
         WebElement username = (WebElement) js.executeScript("return document.querySelector('div#userName').shadowRoot.querySelector('input#kils')");
         username.sendKeys("basuabhinandan99@gmail.com");
 
